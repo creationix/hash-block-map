33646e4cd99a0bde469d91fc9c407254088e5c2c
Tim Caswell <tcaswell@magicleap.com>
2021-04-16 19:55:25 -0500

    Somewhat working write

--- src/main.zig
+++ src/main.zig
@@ -12,29 +12,33 @@ fn AutoMap(comptime BLOCK_POWER: comptime_int) type {
         const BRANCH_POWER = std.math.log2(@sizeOf(Leaf) / @sizeOf(?*u8));
         const BRANCH_FACTOR = 2 << (BRANCH_POWER - 1);
         const Block = [BLOCK_SIZE]u8;
-        const Leaf = struct { block: Block, hash: Hash };
+        const Leaf = struct { block: Block, digest: Digest };
         const Branch = [BRANCH_FACTOR]?*Node;
         const Node = union(enum) { branch: Branch, leaf: Leaf };
 
-        fn emptyBranch() Node {
-            return .{ .branch = .{null} ** BRANCH_FACTOR };
+        fn newBranch(self: *const Self) !*Node {
+            const node = try self.allocator.create(Node);
+            node.* = .{ .branch = .{null} ** BRANCH_FACTOR };
+            return node;
+        }
+
+        fn newLeaf(self: *const Self, block: *const Block, digest: *const Digest) !*Node {
+            const node = try self.allocator.create(Node);
+            node.* = .{ .leaf = .{ .block = block.*, .digest = digest.* } };
+            return node;
         }
 
         allocator: *std.mem.Allocator,
-        root: *Node,
+        root: ?*Node,
 
         pub fn init(comptime allocator: *std.mem.Allocator) !Self {
-            var root = try allocator.create(Node);
-            errdefer self.allocator.destroy(root);
-
-            root.* = emptyBranch();
-            return Self{ .allocator = allocator, .root = root };
+            return Self{ .allocator = allocator, .root = null };
         }
 
         pub fn deinit(self: *Self) void {
-            const root = self.root;
-            self.root.* = undefined;
-            self.allocator.destroy(root);
+            // const root = self.root;
+            // self.root.* = undefined;
+            // self.allocator.destroy(root);
             self.* = undefined;
         }
 
@@ -43,7 +47,7 @@ fn AutoMap(comptime BLOCK_POWER: comptime_int) type {
             return (@intCast(u256, std.mem.bigToNative(u64, digest[0])) << 192) | (@intCast(u256, std.mem.bigToNative(u64, digest[1])) << 128) | (@intCast(u256, std.mem.bigToNative(u64, digest[2])) << 64) | (@intCast(u256, std.mem.bigToNative(u64, digest[3])) << 0);
         }
 
-        pub fn store(self: *Self, block: *const Block, digest: *Digest) void {
+        pub fn store(self: *Self, block: *const Block, digest: *Digest) !void {
 
             // First calculate the SHA256 digest of the block of data.
             sha2.Sha256.hash(block, @ptrCast(*[32]u8, digest), .{});
@@ -52,24 +56,41 @@ fn AutoMap(comptime BLOCK_POWER: comptime_int) type {
             const hash = digestToHash(digest);
 
             var index: u8 = 0;
-            var node: *Node = self.root;
-            while (index < (256 / BRANCH_POWER)) {
-                const slice = getBitSlice(hash, index);
-                const t = switch (node.*) {
-                    Node.branch => |branch| "branch",
-                    Node.leaf => |leaf| "leaf",
-                };
-                std.debug.print("\nindex = {} | slice = 0x{x} | node = {s}\n", .{ index, slice, t });
-                index += 1;
+            if (self.root) |rootNode| {
+                var node: *Node = rootNode;
+                while (index < (256 / BRANCH_POWER)) : (index += 1) {
+                    switch (node.*) {
+                        Node.branch => |*branch| {
+                            const slice = getBitSlice(hash, index);
+                            if (branch[slice]) |childNode| {
+                                std.debug.print("index = {} | Walking down node... | slice = 0x{x}\n", .{ index, slice });
+                                node = childNode;
+                            } else {
+                                branch[slice] = try self.newLeaf(block, digest);
+                                return;
+                            }
+                        },
+                        Node.leaf => |leaf| {
+                            std.debug.print("Found old leaf {x:16} x{:16}\n", .{ leaf.digest[0], leaf.digest[1] });
+                            // TODO: check if existing leaf is same as the one we're writing...
+                            const slice = getBitSlice(hash, index);
+                            std.debug.print("\nindex = {} | converting leaf into branch and two leaves\n", .{index});
+                            var branch = try self.newBranch();
+                            // Move the old leaf to the new branch.
+                            branch.branch[getBitSlice(digestToHash(&leaf.digest), index)] = node;
+                            // Create a new leaf for the new value.
+                            branch.branch[slice] = try self.newLeaf(block, digest);
+                            // Move the pointer to the new branch.
+                            node = branch;
+                        },
+                    }
+                }
+            } else {
+                std.debug.print("Setting new root node as leaf...\n", .{});
+                self.root = try self.newLeaf(block, digest);
+                return;
             }
-            //         inline while (index * Map.BRANCH_POWER < @bitSizeOf(@TypeOf(hash))) {
-            //             const bits = Map.getBitSlice(hash, index);
-            //             std.debug.print("index = {} bits = 0x{x}\n", .{ index, bits });
-            //             index += 1;
-            //         }
-
         }
-
         // Get a slice of bits from the giant hash value.
         fn getBitSlice(hash: Hash, index: u32) u64 {
             const offset = @intCast(u8, index * BRANCH_POWER);
@@ -100,13 +121,16 @@ test "write" {
     var map = try Map.init(std.testing.allocator);
     defer map.deinit();
 
-    const block: Map.Block = .{0} ** Map.BLOCK_SIZE;
+    var block: Map.Block = .{0} ** Map.BLOCK_SIZE;
     var digest: Digest = undefined;
-    map.store(&block, &digest);
+    try map.store(&block, &digest);
     std.debug.print("\ndigest = {*} | {x:8} {x:8} {x:8} {x:8}\n", .{ &digest, digest[0], digest[1], digest[2], digest[3] });
-    const hash = Map.digestToHash(&digest);
-    std.testing.expect(hash == 0xad7facb2586fc6e966c004d7d1d16b024f5805ff7cb47c7a85dabd8b48892ca7);
-    // std.testing.expectEqual(hash, @intCast(u256,0xad7facb2586fc6e966c004d7d1d16b024f5805ff7cb47c7a85dabd8b48892ca7));
+    block[0] = 1;
+    try map.store(&block, &digest);
+    std.debug.print("\ndigest = {*} | {x:8} {x:8} {x:8} {x:8}\n", .{ &digest, digest[0], digest[1], digest[2], digest[3] });
+    block[0] = 2;
+    try map.store(&block, &digest);
+    std.debug.print("\ndigest = {*} | {x:8} {x:8} {x:8} {x:8}\n", .{ &digest, digest[0], digest[1], digest[2], digest[3] });
 }
 
 // test "walk" {
